e164599f1710238e0884b45f7bb630bbd33b980a
IRLadderGoat <bumbi@live.dk>
2020-10-16 13:22:05 +0200

    justified lobby

--- client/lobby/ClientLobby.gd
+++ client/lobby/ClientLobby.gd
@@ -12,3 +12,10 @@ func _on_StartButton_pressed():
 
 func on_start_game():
 	get_tree().change_scene("res://client/game/ClientGame.tscn")
+
+func _on_Lobby_draw():
+	var screen_size = get_viewport().size
+	var amount_teams = $Teams.get_child_count()
+	var team_rect_size = $Teams.get_child(0).get_rect().size.x
+	$Teams.rect_position = Vector2((screen_size.x/2)-((amount_teams*team_rect_size)/1.75), screen_size.y/10)
+	$StartButton.rect_global_position = screen_size-(screen_size/10)
