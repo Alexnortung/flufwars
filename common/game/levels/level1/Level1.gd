1c4d5e82c717edb78fe5f8d010d50331b856fe03
M <34862975+IRLadderGoat@users.noreply.github.com>
2020-10-16 15:51:02 +0200

    Added lobby images and changed how we cache flags

--- common/game/levels/level1/Level1.gd
+++ common/game/levels/level1/Level1.gd
@@ -1,6 +1,11 @@
 extends "res://common/game/levels/LevelInfo.gd"
 
 func _ready():
+	colorDic[0] = createTeamColor("red", "981D1D")
+	colorDic[1] = createTeamColor("green", "40883C")
+	colorDic[2] = createTeamColor("blue", "3F28A7")
+	colorDic[3] = createTeamColor("orange", "B75923")
+
 	teamColors = [Color("981D1D"), Color("40883C"), Color("3F28A7"), Color("B75923")]
 	playersPerTeam = 1
 	teamsInLevel = 4
