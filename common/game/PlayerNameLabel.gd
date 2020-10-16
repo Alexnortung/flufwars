31d0c7bf514580874a4d3475159114b01e3b9c82
IRLadderGoat <bumbi@live.dk>
2020-10-16 13:31:39 +0200

    merge

---
+++ common/game/PlayerNameLabel.gd
@@ -0,0 +1,14 @@
+extends Label
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#$BackgroundNameLabel.rect_size = self.rect_size
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
