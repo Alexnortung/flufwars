308c11a742a139354496ddc72e95410e561fb83b
Alexander Nortung <alex_nortung@live.dk>
2021-03-06 18:21:39 +0100

    added accuracy function and field to guns

--- common/game/weapons/BaseGun.gd
+++ common/game/weapons/BaseGun.gd
@@ -2,3 +2,14 @@ extends "res://common/game/weapons/BaseWeapon.gd"
 
 var projectile = "base_projectile"
 
+# Accuracy: between 0 and 1
+# if accuracy is 0, then the gun will shoot a projectile in a random direction
+#   ± 45 degree from the target
+# if accuracy is 1 it will always shoot straight.
+export(float, 0.0, 1.0)var accuracy : float = 0.99
+
+func add_random_accuracy(direction: Vector2) -> Vector2:
+	var rng = RandomNumberGenerator.new()
+	var max_radians = (1.0 - accuracy) * PI / 4 # degrees
+	var random_accuracy_radians = rng.randf_range(-max_radians, max_radians)
+	return direction.rotated(random_accuracy_radians)
