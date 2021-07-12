7f0bf5e3e2081998093718fe7618fd1f9c3442ef
Alexander Nortung <alex_nortung@live.dk>
2021-07-12 23:47:11 +0200

    added movement to resource splash.

--- client/game/Gameplay/ResourceSplash.gd
+++ client/game/Gameplay/ResourceSplash.gd
@@ -1,5 +1,10 @@
 extends HBoxContainer
 
+var movementVector = Vector2(0, -50)
+var duration = 1
+
+onready var tween = get_node("Tween")
+
 var resourceIcons = [
 	preload("res://assets/resources/emerald.png"),
 	preload("res://assets/resources/diamond.png"),
@@ -17,6 +22,12 @@ func init(position : Vector2, amount : int, type : int):
 		$AmountLabel.add_color_override("font_color", Color(0.1,0.9,0.1))
 	$AmountLabel.text = labelText
 	$TextureRect.texture = resourceIcons[type]
+	call_deferred("start_tween")
+
+func start_tween():
+	tween.interpolate_property(self, "rect_position", self.rect_position, self.rect_position + movementVector, duration)
+	tween.interpolate_callback(self, duration, "destroy")
+	tween.start()
 
 func destroy():
 	queue_free()
