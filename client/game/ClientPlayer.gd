08e956c2b7aecf60cd314af54cf547f952696421
Alexander Nortung <alex_nortung@live.dk>
2021-07-13 23:30:36 +0200

    Players will not send input updates when dead.

--- client/game/ClientPlayer.gd
+++ client/game/ClientPlayer.gd
@@ -1,17 +1,18 @@
 extends "res://common/game/Player.gd"
 
 func _physics_process(delta):
-	if is_network_master():
-		var axis = get_input_axis()
-		if axis != Vector2.ZERO:
-			apply_movement(axis * acc *  delta)
-		apply_friction(acc * delta, axis)
-		motion = move_and_slide(motion)
-		rpc_unreliable_id(1, "network_update", axis)
+	if !is_network_master() or dead:
+		return
+	var axis = get_input_axis()
+	if axis != Vector2.ZERO:
+		apply_movement(axis * acc *  delta)
+	apply_friction(acc * delta, axis)
+	motion = move_and_slide(motion)
+	rpc_unreliable_id(1, "network_update", axis)
 
-		var normalizedDirection = get_normalized_direction()
-		set_look_direction(normalizedDirection)
-		rpc_unreliable_id(1, "on_player_change_direction", normalizedDirection)
+	var normalizedDirection = get_normalized_direction()
+	set_look_direction(normalizedDirection)
+	rpc_unreliable_id(1, "on_player_change_direction", normalizedDirection)
 
 func update_health(newHealth: int):
 	health = newHealth
